85c19531e88833dc843a76413bf1af736b049695
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-29 23:40:01 +0530

    Refactor all the 3 to one generic method and find the maximum

--- src/com/bridgelabz/maingenerics/FindMaximum.java
+++ src/com/bridgelabz/maingenerics/FindMaximum.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.maingenerics;
 
 public class FindMaximum {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Find maximum problems using generics.");
+    // Generic Method
+    public static <T extends Comparable<T>> T maximumOfObject(T first, T second, T third) {
+        T maximumValue = first;
+        if (second.compareTo(maximumValue) > 0)
+            maximumValue = second;
+        if (third.compareTo(maximumValue) > 0)
+            maximumValue = third;
+
+        System.out.println(maximumValue);
+
+        return maximumValue;
     }
 }
