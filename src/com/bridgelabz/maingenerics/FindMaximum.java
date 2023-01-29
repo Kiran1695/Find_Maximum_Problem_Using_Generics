e56360f57390016cfdd509035c42a792d0fef426
Kiran1695 <hindlekarkiran.kh@gmail.com>
2023-01-29 23:59:02 +0530

    Given 3 Integers find the maximum

--- src/com/bridgelabz/maingenerics/FindMaximum.java
+++ src/com/bridgelabz/maingenerics/FindMaximum.java
@@ -1,30 +1,14 @@
 package com.bridgelabz.maingenerics;
 
-public class FindMaximum<T extends Comparable<T>> {
-    T firstValue;
-    T secondValue;
-    T thirdValue;
-
-    public FindMaximum(T firstValue, T secondValue, T thirdValue) {
-        this.firstValue = firstValue;
-        this.secondValue = secondValue;
-        this.thirdValue = thirdValue;
-    }
-
-    public T findMaximumValue() {
-        return FindMaximum.maximumOfObject(firstValue, secondValue, thirdValue);
-    }
-
-    // Generic Method
-    public static <T extends Comparable<T>> T maximumOfObject(T first, T second, T third) {
-        T maximumValue = first;
-        if (second.compareTo(maximumValue) > 0)
-            maximumValue = second;
-        if (third.compareTo(maximumValue) > 0)
-            maximumValue = third;
-
-        System.out.println(maximumValue);
-
+public class FindMaximum {
+    public static Integer maximumInteger(Integer a, Integer b, Integer c) {
+        Integer maximumValue = a;
+        if (b.compareTo(maximumValue) > 0)
+            maximumValue = b;
+        if (c.compareTo(maximumValue) > 0)
+            maximumValue = c;
         return maximumValue;
+
     }
 }
+
